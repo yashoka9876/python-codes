6fd70a5e63ec230192b7de616d75af947157357e
yashoka9876 <98081691+yashoka9876@users.noreply.github.com>
2022-02-08 15:47:20 +0530

    deep code of dictiionary

---
+++ tut6bro.py
@@ -0,0 +1,18 @@
+# print("hai thsi is ashok yadav",end=' ')
+# print("he works at jk technosoft")
+#this is new concept to print in the same line
+# print("hai this is ashok yadav","he works at jk technosoft")
+# print("\t hai \t bro\ man")
+#Here we will explain the concept of dictionary
+d1={"rohan":"burger","james":"nudels","harry":"pani puri",
+    "majesh":{"m":"breakfats","l":"lunch","n":"dinner"},
+    "rajesh":["ram",5,"mahesh baht"],
+    "rahim":("hai",'by',"error",29)}
+
+d1["jassdeep"]="halwa"
+d1["maninder"]="pakore"
+
+print(d1["jassdeep"])
+del d1["majesh"]
+print(d1)
+
